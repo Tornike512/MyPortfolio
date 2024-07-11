d7b18b31c3412f6fcb52f86cb7fafd14c98cea41
Tornike Tsagareishvili <dht5152@gmail.com>
2024-07-11 19:52:11 +0400

    Add: Header Style7127BA

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,16 @@
 export function Header() {
   return (
-    <header>
-      <ul>
-        <li>About</li>
-        <li>Projects</li>
-        <li>Contact</li>
+    <header className="w-full h-[113px] bg-[#1A0B2E] flex justify-center">
+      <ul className="flex items-center justify-between px-[40px] w-full max-w-[1200px] text-[#ffffff]">
+        <li className="text-[20px] font-bold cursor-pointer hover:text-[#7127BA]">
+          About
+        </li>
+        <li className="text-[20px] font-bold cursor-pointer hover:text-[#7127BA]">
+          Projects
+        </li>
+        <li className="text-[20px] font-bold cursor-pointer hover:text-[#7127BA]">
+          Contact
+        </li>
       </ul>
     </header>
   );
