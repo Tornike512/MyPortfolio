fc0c61a99f970f954a8d2d11699a4b2f9d18f088
Tornike Tsagareishvili <dht5152@gmail.com>
2024-07-13 15:19:54 +0400

    Add: Skills Section HTML

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Header } from "./components/header";
 import { PersonInfo } from "./components/header/personInfo";
+import { Skills } from "./components/header/skills";
 
 function App() {
   return (
     <main className="flex flex-col items-center justify-center">
       <Header />
-      <PersonInfo />
+      <PersonInfo />/
+      <Skills />
     </main>
   );
 }
---
+++ src/components/header/skills/Skills.tsx
@@ -0,0 +1,26 @@
+export function Skills() {
+  return (
+    <section className="max-w-[1200px] w-full py-[120px] px-[40px]">
+      <h2>Looking for Front-End Developer Roles</h2>
+      <p>My Skills Include:</p>
+      <figure>
+        <img src="" alt="Typescript Logo" />
+        <img src="" alt="React Logo" />
+        <img src="" alt="Sass Logo" />
+        <img src="" alt="Ant Design Logo" />
+        <img src="" alt="Tailwind Logo" />
+        <img src="" alt="Redux Logo" />
+      </figure>
+      <figure>
+        <img src="" alt="React Query Logo" />
+        <img src="" alt="React Native Logo" />
+        <img src="" alt="Postman Logo" />
+        <img src="" alt="Figma Logo" />
+        <img src="" alt="Adobe Photoshop Logo" />
+        <img src="" alt="GraphQl Logo" />
+      </figure>
+    </section>
+  );
+}
+
+export default Skills;
