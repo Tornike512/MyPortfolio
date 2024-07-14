20690228970452555e99cbe220bad1cbb33388a7
Tornike Tsagareishvili <dht5152@gmail.com>
2024-07-14 17:06:51 +0400

    Add: Project Section HTML

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Header } from "./components/header";
 import { PersonInfo } from "./components/header/personInfo";
+import { ProjectsSection } from "./components/header/projects";
 import { Skills } from "./components/header/skills";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <PersonInfo />/
       <Skills />
+      <ProjectsSection />
     </main>
   );
 }
---
+++ src/components/header/projects/ProjectsSection.tsx
@@ -0,0 +1,29 @@
+export function ProjectsSection() {
+  return (
+    <section>
+      <ul>
+        <li>
+          <h2>project name</h2>
+          <p>project description</p>
+        </li>
+        <img src="" alt="Project Image" />
+      </ul>
+      <ul>
+        <li>
+          <h2>project name</h2>
+          <p>project description</p>
+        </li>
+        <img src="" alt="Project Image" />
+      </ul>
+      <ul>
+        <li>
+          <h2>project name</h2>
+          <p>project description</p>
+        </li>
+        <img src="" alt="Project Image" />
+      </ul>
+    </section>
+  );
+}
+
+export default ProjectsSection;
