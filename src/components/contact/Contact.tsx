39e19f4d3609408040468d3529f60a2578a0c25b
Tornike Tsagareishvili <dht5152@gmail.com>
2024-07-14 20:06:16 +0400

    Add: Contact Section Style

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,12 +1,28 @@
+import whiteLinkedin from "src/assets/white-linkedin.png";
+import whiteGithub from "src/assets/white-github.png";
+
 export function Contact() {
   return (
-    <footer>
-      <h2>Contact</h2>
-      <p>Email: torniketsagareishvili64@gmail.com</p>
-      <p>
-        Linkedin: https://www.linkedin.com/in/tornike-tsagareishvili-9a2790280/
+    <footer className="px-[40px] py-[120px] text-[#ffffff] w-full max-w-[1200px]">
+      <h2 className="font-bold text-[34px] mb-[70px]">Contact:</h2>
+      <p className="text-[18px] mb-[20px]">
+        Email: torniketsagareishvili64@gmail.com
       </p>
-      Github: https://github.com/Tornike512
+      <nav className="flex items-center">
+        <a target="_blank" href="https://github.com/Tornike512">
+          <img
+            className="w-[50px]"
+            src={whiteLinkedin}
+            alt="White Linkedin Logo"
+          />
+        </a>
+        <a
+          target="_blank"
+          href="https://www.linkedin.com/in/tornike-tsagareishvili-9a2790280/"
+        >
+          <img className="w-[50px]" src={whiteGithub} alt="White Github Logo" />
+        </a>
+      </nav>
     </footer>
   );
 }
