fa66800294db6dd9603258d159d739c6247ca6cc
Tornike Tsagareishvili <dht5152@gmail.com>
2024-07-15 22:44:14 +0400

    Add: Skills Section Responsive

--- src/components/personInfo/PersonInfo.tsx
+++ src/components/personInfo/PersonInfo.tsx
@@ -2,7 +2,7 @@ import personImage from "assets/person-image.jpg";
 
 export function PersonInfo() {
   return (
-    <section className="flex flex-col items-center justify-center max-w-[1200px] w-full relative py-[120px] px-[40px] text-[#ffffff]">
+    <section className="flex flex-col items-center justify-center max-w-[1200px] w-full relative py-[120px] px-[40px] text-[#ffffff] md:pb-0">
       <figure className="flex items-center justify-center w-full lg:flex-col">
         <img
           className="w-[200px] rounded-[100%] mr-[40px] shadow-[0_0_187px_#bf7eff] lg:mr-0"
@@ -13,12 +13,12 @@ export function PersonInfo() {
           <p className="text-[20px] text-[#7127BA] lg:mt-[10px] lg:flex lg:justify-center">
             Front-End Developer
           </p>
-          <h1 className="whitespace-nowrap md:text-[35px] md:mb-[20px]">
+          <h1 className="whitespace-nowrap md:text-[35px] md:mb-[20px] sm:text-[28px]">
             Tornike Tsagareishvili
           </h1>
         </figcaption>
       </figure>
-      <p className="absolute bottom-[-12%] w-full text-[18px] font-bold text-center md:relative">
+      <p className="absolute bottom-[-12%] w-full text-[18px] font-bold text-center md:relative lg:px-[40px] md:px-[10px]">
         I'm a front-end developer with over 2 years of experience. I focus on
         creating user-friendly interfaces and seamless user experiences. I've
         worked closely with backend developers on projects, using tools like
--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -12,68 +12,68 @@ import graphQlLogo from "src/assets/graphql-logo.svg";
 
 export function Skills() {
   return (
-    <section className="flex items-center justify-center flex-col max-w-[1200px] w-full py-[120px] px-[40px]">
-      <h2 className="text-[24px] text-[#ffffff] font-bold">
+    <section className="flex items-center justify-center flex-col max-w-[1200px] w-full py-[120px] px-[40px] md:pb-0">
+      <h2 className="text-[24px] text-[#ffffff] font-bold whitespace-nowrap sm:text-[18px]">
         Looking for Front-End Developer Roles
       </h2>
       <p className="mb-[80px] text-[17px] text-[#ffffff] font-bold">
         My Skills Include:
       </p>
-      <figure className="grid grid-cols-[1fr_1fr_1fr_1fr_1fr_1fr] gap-x-[80px]">
+      <figure className="grid grid-cols-[1fr_1fr_1fr_1fr_1fr_1fr] gap-x-[80px] lg:grid-cols-[1fr_1fr_1fr] lg:gap-y-[60px] ">
         <img
-          className="w-[120px] cursor-pointer"
+          className="w-[100%] cursor-pointer"
           src={typescriptLogo}
           alt="Typescript Logo"
         />
         <img
-          className="w-[120px] cursor-pointer"
+          className="w-[100%] cursor-pointer"
           src={reactLogo}
           alt="React Logo"
         />
         <img
-          className="w-[120px] cursor-pointer"
+          className="w-[100%] cursor-pointer"
           src={sassLogo}
           alt="Sass Logo"
         />
         <img
-          className="w-[120px] cursor-pointer"
+          className="w-[100%] cursor-pointer"
           src={antDesignLogo}
           alt="Ant Design Logo"
         />
         <img
-          className="w-[120px] cursor-pointer"
+          className="w-[100%] cursor-pointer"
           src={tailwindLogo}
           alt="Tailwind Logo"
         />
         <img
-          className="w-[120px] cursor-pointer"
+          className="w-[100%] cursor-pointer"
           src={reduxLogo}
           alt="Redux Logo"
         />
       </figure>
       <figure className="flex justify-center items-center mt-[100px]">
         <img
-          className="w-[120px] cursor-pointer mr-[80px]"
+          className="w-[10%] lg:w-[19%] cursor-pointer mr-[80px] lg:mr-[20px]"
           src={reactQueryLogo}
           alt="React Query Logo"
         />
         <img
-          className="w-[120px] cursor-pointer mr-[80px]"
+          className="w-[10%] lg:w-[19%] cursor-pointer mr-[80px] lg:mr-[20px]"
           src={postmanLogo}
           alt="Postman Logo"
         />
         <img
-          className="w-[120px] cursor-pointer mr-[80px]"
+          className="w-[10%] lg:w-[19%] cursor-pointer mr-[80px] lg:mr-[20px]"
           src={figmaLogo}
           alt="Figma Logo"
         />
         <img
-          className="w-[120px] cursor-pointer mr-[80px]"
+          className="w-[10%] lg:w-[19%] cursor-pointer mr-[80px] lg:mr-[20px]"
           src={adobePhotoshopLogo}
           alt="Adobe Photoshop Logo"
         />
         <img
-          className="w-[120px] cursor-pointer"
+          className="w-[10%] lg:w-[19%] cursor-pointer"
           src={graphQlLogo}
           alt="GraphQl Logo"
         />
